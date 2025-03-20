e5c284caf0bf5e4007c38eb426b3f1aadd539a7e
SHOURYA-SINHA <jmditolima@gmail.com>
2025-03-20 20:55:20 +0700

    Updates

---
+++ assets/Login-DNpPnKLs.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CINQfK7z.js";const t=()=>o.jsx("div",{children:"Login"});export{t as default};
---
+++ assets/Register-BkDub5Up.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CINQfK7z.js";const t=()=>e.jsx("div",{children:"Register"});export{t as default};
